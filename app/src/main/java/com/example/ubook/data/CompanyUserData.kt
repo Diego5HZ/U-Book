489a55d818714cfd6fb14f8cea99adbf2b736aee
Diego5HZ <49615431+Diego5HZ@users.noreply.github.com>
2022-04-21 18:59:31 -0500

    Revert "Feat login/signup"

--- app/src/main/java/com/example/ubook/data/CompanyUserData.kt
+++
@@ -1,3 +0,0 @@
-package com.example.ubook.data
-
-data class CompanyUserData(val companyName:String? = null,val typeService:String? = null,val tags:String? = null,val firstDateService:String? = null,val email:String? = null, val password:String? = null,val logo:String? = null,val lei:String? = null)
