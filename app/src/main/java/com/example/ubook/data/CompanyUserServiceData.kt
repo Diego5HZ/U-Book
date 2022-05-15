2960753978d95fdb42891a4365eaa9aabbc04c1c
Khumoyun Rakhimov <huma.rahimov@gmail.com>
2022-05-16 02:19:34 +0300

    Add Service Company
    Can you check it Diegou

--- app/src/main/java/com/example/ubook/data/CompanyUserServiceData.kt
+++ app/src/main/java/com/example/ubook/data/CompanyUserServiceData.kt
@@ -1,3 +1,3 @@
 package com.example.ubook.data
 //?Place
-data class ServiceCompanyUserData(val Place:String? = null,val Description:String? = null,val Country:String? = null, val City:String? = null, val Address:String? = null,val Workinghours:String? = null,val Contact:String? = null, val Motivationalphrase:String? = null, val Status:String? = null)
+data class ServiceCompanyUserData(val Place:String? = null,val Description:String? = null,val Country:String? = null, val City:String? = null, val Address:String? = null,val Workinghours:String? = null,val Contact:String? = null, val Motivationalphrase:String? = null, val Status:String? = null, val email:String? = null)
--- app/src/main/java/com/example/ubook/fragments/AddServiceCompanyUserFragment.kt
+++ app/src/main/java/com/example/ubook/fragments/AddServiceCompanyUserFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.ubook.R
+import com.example.ubook.data.CommonUserData
+import com.example.ubook.data.ServiceCompanyUserData
 import com.example.ubook.databinding.FragmentAddServiceCompanyUserBinding
 import com.example.ubook.databinding.FragmentSettingsCommonUserBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -29,7 +31,32 @@ class AddServiceCompanyUserFragment : Fragment() {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance()
 
-        //--Insert actions here--//
+
+
+        private var Place = ""
+        private var Description =""
+        private var Country=""
+        private var City=""
+        private var Address=""
+        private var Workinghours = ""
+        private var Contact = ""
+        private var Motivationalphrase = ""
+        private var Status = ""
+        private var email = ""
+
+        private fun storeData() {
+            val newEmail = email.lowercase().replace('.',' ')
+            val servicecompanyuser = ServiceCompanyUserData(Place,email,Description,Country, City, Address, Workinghours, Contact, Motivationalphrase, Status)
+            //tag of the profile
+            database.child("service_company_user").child(email).setValue(servicecompanyuser)
+        }
+
+        binding.saveBtn.setOnClickListener {
+            val firebaseUser = firebaseAuth.currentUser
+//user not null, user is logged in, get user info
+            val email = firebaseUser?.email
+            val newEmail = email?.lowercase()?.replace('.',' ')
+        }
 
 
         // Inflate the layout for this fragment
