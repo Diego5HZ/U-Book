07cb1b6b8b90b49ea98ed33262e4e71f7c0d8894
DiegoHZ5 <safairus@gmail.com>
2022-05-16 10:42:35 +0300

    completed add service
    no service has an id

--- app/src/main/java/com/example/ubook/data/CompanyUserServiceData.kt
+++ app/src/main/java/com/example/ubook/data/CompanyUserServiceData.kt
@@ -1,3 +1,3 @@
 package com.example.ubook.data
-//?Place
-data class ServiceCompanyUserData(val Place:String? = null,val Description:String? = null,val Country:String? = null, val City:String? = null, val Address:String? = null,val Workinghours:String? = null,val Contact:String? = null, val Motivationalphrase:String? = null, val Status:String? = null, val email:String? = null)
+
+data class ServiceCompanyUserData(val Place:String? = null,val Description:String? = null,val Country:String? = null, val City:String? = null, val Address:String? = null,val Weekdays:String? = null,val Weekend:String? = null,val Contact:String? = null, val MotivationalPhrase:String? = null, val Status:Boolean? = null, val email:String? = null)
